3284168b83f7ae4415f494c8ca33797e1370f42b
Antti Koivurova <antti.koivurova@helsinki.fi>
2017-08-03 23:05:52 +0300

    viikko 2

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,11 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Kayttaja (nimi, liittymispaiva, salasana) VALUES ('Abraham Lincoln', '2011-05-16 15:36:38', 'straitpimpin');
+INSERT INTO Kayttaja (nimi, liittymispaiva, salasana) VALUES ('haloodsgklngdsnk', '2013-04-11 15:36:38', 'salasana');
+
+INSERT INTO Keskustelualue (nimi) VALUES ('Miten?');
+INSERT INTO Keskustelualue (nimi) VALUES ('Miksi?');
+
+INSERT INTO Tagi (nimi) VALUES ('Asiaton');
+INSERT INTO Tagi (nimi) VALUES ('Asd');
+
+INSERT INTO Viesti (sisalto, lahetysaika) VALUES ('En tiedä', '2012-01-11 15:26:38');
+INSERT INTO Viesti (sisalto, lahetysaika) VALUES ('Apua, olen tyhmä paksukainen', '2000-01-11 00:40:38');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,29 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Kayttaja(
+	kayttajaId SERIAL PRIMARY KEY,
+	nimi varchar(30) NOT NULL UNIQUE,
+	liittymispaiva timestamp,
+	salasana varchar(50) NOT NULL
+);
+
+CREATE TABLE Keskustelualue(
+	keskustelualueId SERIAL PRIMARY KEY,
+	nimi varchar(40) NOT NULL
+);
+
+CREATE TABLE Tagi(
+	tagiId SERIAL PRIMARY KEY,
+	nimi varchar(10) NOT NULL
+);
+
+CREATE TABLE Viesti(
+	viestiId SERIAL PRIMARY KEY,
+	sisalto varchar(300),
+	lahetysaika timestamp,
+	kirjoittajaId INTEGER REFERENCES Kayttaja(kayttajaId),
+	keskustelualueId INTEGER REFERENCES Keskustelualue(keskustelualueId)
+);
+
+CREATE TABLE Tagays(
+	viestiId INTEGER REFERENCES Viesti(viestiId),
+	tagiId INTEGER REFERENCES Tagi(tagiId)
+);
